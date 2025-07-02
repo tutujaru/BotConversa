d75125c09a83961dd19c4c823739b168cf04058a
tutujaru <100329749+tutujaru@users.noreply.github.com>
2025-07-02 08:06:12 -0400

    Update MainListItems.js

--- frontend/src/layout/MainListItems.js
+++ frontend/src/layout/MainListItems.js
@@ -469,7 +469,7 @@ const MainListItems = (props) => {
         textAlign: "right", 
         fontWeight: "bold" 
       }}>
-        Versão:4.0.8 - CRM - RO © 2025
+        Versão:5.0.0 - CRM - RO © 2025
       </Typography>
     </>
   )}
